84d33d1750d7da1711487b51ed37b667dd8b289a
chetanhosmani <chetanhosmani0@gmail.com>
2012-06-09 02:59:20 +0530

    Commit:Update structure Add sendHandshake method. Other methods to be added as needed.

--- src/freenet/node/OutgoingStreamMangler.java
+++ src/freenet/node/OutgoingStreamMangler.java
@@ -1,5 +1,18 @@
 package freenet.node;
-
+/**
+ * This class is analogous to the OutgoingPacketMangler.
+ * Most of the code for the implementation should be taken from FNPPacketMangler class as we don't want to change
+ * the protocol for handshaking and processing. However we need to keep in mind that they use streams and it 
+ * should be relatively simpler implementing the JFK protocol.
+ * @author chetan
+ *
+ */
 public interface OutgoingStreamMangler {
+
+	/**
+	 * Send a handshake, if possible, to the node.
+	 * @param pn
+	 */
+	public void sendHandshake(PeerNode pn, boolean notRegistered);
 
 }
